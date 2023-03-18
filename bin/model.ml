3f1824a4cf177fd2885f43373f57691c4b1b3c4d
Ambre Austen Suhamy <ambre@tarides.com>
2023-03-18 20:11:25 +0100

    Now with a bit of colours!

--- bin/model.ml
+++ bin/model.ml
@@ -1,24 +1,16 @@
 type col = Left | Middle | Right
 
 type t = {
-  holding : (int * col) option;
+  holding : (int * col * col option) option;
   moves : int;
   left : int list;
   middle : int list;
   right : int list;
-  error : col option;
 }
 
 let init n =
   let rec aux acc = function 0 -> acc | n -> aux (n :: acc) (n - 1) in
-  {
-    holding = None;
-    moves = 0;
-    left = aux [] n;
-    middle = [];
-    right = [];
-    error = None;
-  }
+  { holding = None; moves = 0; left = aux [] n; middle = []; right = [] }
 
 let is_win = function
   | { left = []; middle = []; holding = None; _ } -> true
@@ -27,51 +19,36 @@ let is_win = function
 let droppable x = function [] -> true | y :: _ -> x < y
 
 let move col state =
-  let state = { state with error = None } in
   match (col, state) with
   (* Taking a disk *)
   | Left, { holding = None; left = x :: xs; _ } ->
-      { state with holding = Some (x, Left); left = xs }
+      { state with holding = Some (x, Left, None); left = xs }
   | Middle, { holding = None; middle = x :: xs; _ } ->
-      { state with holding = Some (x, Middle); middle = xs }
+      { state with holding = Some (x, Middle, None); middle = xs }
   | Right, { holding = None; right = x :: xs; _ } ->
-      { state with holding = Some (x, Right); right = xs }
+      { state with holding = Some (x, Right, None); right = xs }
   (* Cancelling a move *)
-  | Left, { holding = Some (n, Left); _ } ->
+  | Left, { holding = Some (n, Left, _); _ } ->
       { state with holding = None; left = n :: state.left }
-  | Middle, { holding = Some (n, Middle); _ } ->
+  | Middle, { holding = Some (n, Middle, _); _ } ->
       { state with holding = None; middle = n :: state.middle }
-  | Right, { holding = Some (n, Right); _ } ->
+  | Right, { holding = Some (n, Right, _); _ } ->
       { state with holding = None; right = n :: state.right }
   (* Normal moves *)
-  (*   L -> M *)
-  | Middle, { holding = Some (x, Left); middle; _ } when droppable x middle ->
+  (*   -> L *)
+  | Left, { holding = Some (x, _, _); left; _ } when droppable x left ->
+      { state with holding = None; moves = state.moves + 1; left = x :: left }
+  (*   -> M *)
+  | Middle, { holding = Some (x, _, _); middle; _ } when droppable x middle ->
       {
         state with
         holding = None;
         moves = state.moves + 1;
         middle = x :: state.middle;
       }
-  (*   L -> R *)
-  | Right, { holding = Some (x, Left); right; _ } when droppable x right ->
+  (*   -> R *)
+  | Right, { holding = Some (x, _, _); right; _ } when droppable x right ->
       { state with holding = None; moves = state.moves + 1; right = x :: right }
-  (*   M -> L *)
-  | Left, { holding = Some (x, Middle); left; _ } when droppable x left ->
-      { state with holding = None; moves = state.moves + 1; left = x :: left }
-  (*   M -> R *)
-  | Right, { holding = Some (x, Middle); right; _ } when droppable x right ->
-      { state with holding = None; moves = state.moves + 1; right = x :: right }
-  (*   R -> L *)
-  | Left, { holding = Some (x, Right); left; _ } when droppable x left ->
-      { state with holding = None; moves = state.moves + 1; left = x :: left }
-  (*   R -> M *)
-  | Middle, { holding = Some (x, Right); middle; _ } when droppable x middle ->
-      {
-        state with
-        holding = None;
-        moves = state.moves + 1;
-        middle = x :: middle;
-      }
   (* Impossible moves *)
   (*   Taking from an empty column *)
   | Left, { holding = None; left = []; _ }
@@ -79,7 +56,8 @@ let move col state =
   | Right, { holding = None; right = []; _ } ->
       state
   (*  Moving a disk on a lower disk. Forbidden! *)
-  | col, { holding = Some _; _ } -> { state with error = Some col }
+  | c2, { holding = Some (n, c1, _); _ } ->
+      { state with holding = Some (n, c1, Some c2) }
 
 let col_of_arrow = function
   | `Left -> Left
--- bin/view.ml
+++ bin/view.ml
@@ -8,46 +8,49 @@ let hmul i n =
   let rec aux acc n = if n < 2 then acc else aux I.(i <|> acc) (n - 1) in
   aux i n
 
-let pipe = I.string A.empty "┃"
+let pipe = I.string A.(fg black) "┃"
 
-let disk_of_n ?(error = false) ?(holding = false) n =
+let disk_of_n ?(error = false) n =
   let attr = if error then A.fg A.red else A.empty in
-  let char =
-    if n < 10 then I.strf ~attr "%d" n
-    else I.char attr (Char.chr @@ (Char.code 'A' + n - 10)) 1 1
-  in
-  let disk = hmul char ((2 * n) + 1) in
-  I.(hsnap (width disk) (if holding then empty else pipe) </> disk)
+  let c = I.string attr "\u{2588}" in
+  I.(
+    string attr "\u{1FB6E}" <|> hmul c ((2 * n) + 1) <|> string attr "\u{1FB6C}")
 
-let img_of_col (w, h) l =
-  let rec aux acc = function
+let img_of_col ?(error = false) (w, h) l =
+  let rec aux acc first = function
     | [] -> acc
-    | n :: l -> aux I.(acc <-> hsnap (w / 3) (disk_of_n n)) l
+    | n :: l ->
+        aux
+          I.(acc <-> hsnap (w / 3) (disk_of_n ~error:(first && error) n))
+          false l
   in
-  let disks = aux I.empty l in
+  let disks = aux I.empty true l in
   let pole = I.hsnap (w / 3) (vmul pipe (h - I.height disks - 1)) in
   I.(pole <-> disks)
 
-let img_of_state (w, h)
-    ({ holding; moves; left; middle; right; error } : Model.t) =
-  let cols =
+let img_of_state ((w, _) as dims)
+    ({ holding; moves; left; middle; right } : Model.t) =
+  let move_str = I.strf "Moves: %d" moves in
+  let top_line =
     I.(
-      img_of_col (w, h) left
-      <|> img_of_col (w, h) middle
-      <|> img_of_col (w, h) right)
+      move_str
+      </> hsnap w
+            (match holding with
+            | None -> I.empty
+            | Some (n, _, None) ->
+                I.(string A.empty "Holding: " <|> disk_of_n n)
+            | Some (n, _, Some _) ->
+                I.(string A.empty "Holding: " <|> disk_of_n ~error:true n)))
   in
-  match (holding, error) with
-  | None, None -> I.(strf "Moves: %d" moves <-> cols)
-  | Some (n, _), None ->
-      I.(
-        strf "Moves: %d" moves
-        </> hsnap w (string A.empty "Holding: " <|> disk_of_n ~holding:true n)
-        <-> cols)
-  | Some (n, _), Some _ ->
-      I.(
-        strf "Moves: %d" moves
-        </> hsnap w
-              (string A.empty "Holding: "
-              <|> disk_of_n ~error:true ~holding:true n)
-        <-> cols)
-  | None, Some _ -> failwith "Impossible case!"
+  let l, m, r =
+    (img_of_col dims left, img_of_col dims middle, img_of_col dims right)
+  in
+  let l, m, r =
+    match holding with
+    | Some (_, _, Some Left) -> (img_of_col ~error:true dims left, m, r)
+    | Some (_, _, Some Middle) -> (l, img_of_col ~error:true dims middle, r)
+    | Some (_, _, Some Right) -> (l, m, img_of_col ~error:true dims right)
+    | None | Some (_, _, None) -> (l, m, r)
+  in
+  let cols = I.(l <|> m <|> r) in
+  I.(top_line <-> cols)
